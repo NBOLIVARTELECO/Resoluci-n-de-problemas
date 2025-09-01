5e0a174ac2e301efaf03b1ad9cfda42fcbbb3ee2
Nestor <nbolivar@unal.edu.co>
2025-09-01 11:43:29 -0500

    Explicacion del if

--- Test.py
+++ Test.py
@@ -13,14 +13,22 @@
 
 # AX = B - Z
 
+
 mensaje = "El valor de la variables es"
 #iMPLEMENTAR LA FUNCION INPUT, Y EXPLICAR CADA PARAMETRO EFECTUADO
 #A = input("Escriba un numero parar la variable A ")
 A = int("10")
 
-print(type(A), A)
+print(type(A), " y la variable valor",A)
 #Convetir los tipos de datos Float, Int, String entre si
 result = A + A
+
+# Estructura de control, Operaciones logicas
+#Booleano, 1,0, 
+
+if(A>5):
+    print("este es el resultado del if vverdadero")
+    print(f"Lavariable a es igual a: {A}")
 
 print(type(result), result)
 
