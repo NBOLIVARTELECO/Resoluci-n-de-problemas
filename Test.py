9d516b9a81a11c984cdf6ebaafad38129a463f4b
Nestor <nbolivar@unal.edu.co>
2025-09-01 11:25:57 -0500

    first commit

---
+++ Test.py
@@ -0,0 +1,30 @@
+#Autor
+
+#
+#print("Hello World")
+
+# definicion a problemas
+
+# solucion a problemas
+
+#Algoritmos
+
+# encuentre el valor de x en la siguiente ecuacion
+
+# AX = B - Z
+
+mensaje = "El valor de la variables es"
+#iMPLEMENTAR LA FUNCION INPUT, Y EXPLICAR CADA PARAMETRO EFECTUADO
+#A = input("Escriba un numero parar la variable A ")
+A = int("10")
+
+print(type(A), A)
+#Convetir los tipos de datos Float, Int, String entre si
+result = A + A
+
+print(type(result), result)
+
+
+
+
+
